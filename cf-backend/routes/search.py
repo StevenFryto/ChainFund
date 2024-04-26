7fae0db5d9d9664cc991fd5a30f8ec749a1a91db
Sw1PerGoSwiPinG <zjy1146161876@outlook.com>
2024-04-27 00:44:56 +0800

    change the connection method with mysql in search.py

--- cf-backend/routes/search.py
+++ cf-backend/routes/search.py
@@ -1,28 +1,19 @@
 from flask import Flask, request, jsonify, Blueprint
 import pymysql
 
-# 创建 Flask 应用
-app = Flask(__name__)
+from config.config import DB_CONFIG
 
 # 创建一个蓝图
 search_bp = Blueprint("search", __name__)
 
+# 连接数据库
+connection = pymysql.connect(**DB_CONFIG)
 
 @search_bp.route("/search", methods=["GET"])
 def search():
     query = request.args.get("query", "")
     if not query:
         return jsonify([])
-
-    connection = pymysql.connect(
-        host="localhost",
-        user="root",
-        port=3306,
-        password="Doncic77++",
-        db="chainfund",
-        charset="utf8mb4",
-        cursorclass=pymysql.cursors.DictCursor,
-    )
     try:
         with connection.cursor() as cursor:
             sql = "SELECT id, title FROM project WHERE title LIKE %s"
